83be685ccbe7f320efefbc76d78cb5d422e85f8a
Justin Davis <justin@chartio.com>
2014-12-16 00:30:42 -0800

    latest

--- periodic.py
+++ periodic.py
@@ -33,29 +33,45 @@ def get_word_clues_dict():
 
 
 def main():
-    if len(sys.argv) == 2:
-        num = int(sys.argv[1])
-    else:
-        print >> sys.stderr, 'Please enter number of subsitutions desired'
-        sys.exit(1)
-    assert num > 0
+    #if len(sys.argv) == 2:
+    #    num = int(sys.argv[1])
+    #else:
+    #    print >> sys.stderr, 'Please enter number of subsitutions desired'
+    #    sys.exit(1)
+    #assert num > 0
+    ltr = sys.argv[1]
 
     words = get_all_words()
     symbols = get_periodic_symbols()
     ny_clues = get_word_clues_dict()
 
+    words_set = set(words)
+
+    possibles = []
     for word in words:
         word_copy = word
         matched_symbols = []
         num_substitutions = 0
         for sym in symbols:
+            if sym != ltr:
+                continue
             count = word.count(sym)
             if count:
                 num_substitutions += count
                 matched_symbols.append(sym)
                 word = word.replace(sym, '')
-        if not word and num_substitutions == num and word_copy in ny_clues:
-            print word_copy, matched_symbols, ny_clues[word_copy]
+                if word in words_set and num_substitutions == 1:
+                    possibles.append((word, word_copy, ny_clues.get(word), ny_clues.get(word_copy)))
+                    break
+
+    def sort(a, b):
+        return -cmp(len(a[0]) + len(a[1]), len(b[0]) + len(b[1]))
+    possibles.sort(sort)
+    for p in possibles:
+        print p
+        print
+        #if not word and num_substitutions == num and word_copy in ny_clues:
+        #    print word_copy, matched_symbols, ny_clues[word_copy]
 
 
 if __name__ == '__main__':
